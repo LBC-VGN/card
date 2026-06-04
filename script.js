c64ea43e4c02bc363c4db1f2058f1d7b5fc37190
LBC <depotsyslbc@gmail.com>
2026-06-04 11:33:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // Levantamos los datos limpitos de la pantalla
     const nombre = document.getElementById('txt-nombre').innerText.trim();
     const cargo = document.getElementById('txt-cargo').innerText.trim();
     const telefono = document.getElementById('link-tel').innerText.replace(/\s+/g, '');
     const email = document.getElementById('link-mail').innerText.trim();
 
-    // Estructura VCard ultra-compatible con iPhone (iOS) y Android
     const vcard = [
         'BEGIN:VCARD',
         'VERSION:3.0',
-        `FN:${nombre}`,        // Nombre completo completo
-        `N:;${nombre};;;`,     // Estructura de apellido/nombre que pide iPhone
-        'ORG:La Bella Cucina', // Nombre de la empresa
-        `TITLE:${cargo}`,      // Cargo (Director, etc.)
-        `TEL;TYPE=CELL:${telefono}`, // Le avisamos al iPhone que es un celular
+        `FN:${nombre}`,
+        `N:;${nombre};;;`,
+        'ORG:La Bella Cucina',
+        `TITLE:${cargo}`,
+        `TEL;TYPE=CELL:${telefono}`,
         `EMAIL;TYPE=INTERNET:${email}`,
         'END:VCARD'
     ].join('\n');
     
     new QRious({
         element: document.getElementById('qr'),
-        size: 450, // Tu súper resolución HD
-        background: '#003840',
-        foreground: '#909EAE',
-        level: 'M',
+        size: 600,           // Subimos a 600 para que al reescalar con CSS a 150px sobremuree y se vea HD
+        level: 'H',          // Nivel High de corrección de errores (por si el platino brilla mucho)
+        background: '#003840', // Seteamos el MISMO azul del CSS para que no haya bordes fantasma
+        foreground: '#909EAE', // El Platino oficial de Doña Marta. Contraste puro sobre el azul
         value: vcard
     });
 });
